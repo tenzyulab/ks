b337ca5546cc4c0fbb1e099c0e28ce8108579834
tenzyu <tenzyu.on@gmail.com>
2022-12-12 14:40:03 +0900

    chore: put configurations

---
+++ build.js
@@ -0,0 +1,26 @@
+import { build } from 'esbuild'
+
+////////////////////////////////////////////////////////////////
+
+const flags = new Set(process.argv.filter((arg) => arg.startsWith('--')))
+const isProd = process.env['APP_ENV'] === 'prod'
+
+// https://esbuild.github.io/api/
+const options = {
+  legalComments: 'eof',
+  drop: isProd ? ['console', 'debugger'] : [],
+  minify: flags.has('--minify') || isProd,
+  color: true,
+  logLevel: 'warning',
+
+  /* Build Options */
+  bundle: true,
+  outfile: './dist/index.js',
+  entryPoints: ['./src/index.ts'],
+  watch: flags.has('--watch'),
+  platform: 'node',
+  target: 'es2022',
+  format: 'esm',
+}
+
+build(options).catch(() => process.exit(1))
