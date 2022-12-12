03ea77733250cefe668ca518673774baeee952d4
tenzyu (/ˈtenʑɯ/) <tenzyu.on@gmail.com>
2022-12-12 17:32:13 +0900

    fix: add templates (#4)

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import { VITE_REACT } from './templates'
 ;(async () => {
   const basepath = process.argv[2] ?? '.'
 
-  await $`mkdir -p ${basepath}`
+  await $`mkdir -p ${basepath}/public`
+  await $`mkdir -p ${basepath}/src/assets`
   Object.entries(VITE_REACT).forEach(([filename, content]) => {
     $`echo ${content} > ${basepath}/${filename}`
   })
